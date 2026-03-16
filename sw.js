1ea543282152bd788db82ffcf01e9c8e6446cebe
EPKolar <planung@ep-kolar.at>
2026-03-16 21:05:46 +0100

    v3.2.3: SW version update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.2.2
-const CACHE_NAME = 'epkolar-v3.2.2';
+// EP Kolar Service Worker v3.2.3
+const CACHE_NAME = 'epkolar-v3.2.3';
 const ASSETS = [
   './',
   './index.html'
