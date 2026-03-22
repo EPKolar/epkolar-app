d61f1d1eaf1d92c86a9314836a619a0e5a633b92
EPKolar <planung@ep-kolar.at>
2026-03-22 20:01:34 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.5.14
-const CACHE_NAME = 'epkolar-v3.5.14';
+// EP Kolar Service Worker v3.5.15
+const CACHE_NAME = 'epkolar-v3.5.15';
 const ASSETS = [
   './',
   './index.html'
