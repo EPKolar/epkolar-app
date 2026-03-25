1573e7239037a0c076d53c6229c8b76cf9dbc78f
EPKolar <planung@ep-kolar.at>
2026-03-25 12:28:56 +0100

    v3.5.42: sw.js version bump

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.5.41
-const CACHE_NAME = 'epkolar-v3.5.41';
+// EP Kolar Service Worker v3.5.42
+const CACHE_NAME = 'epkolar-v3.5.42';
 const ASSETS = [
   './',
   './index.html'
