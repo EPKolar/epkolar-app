78b320b01819cb638702b78c91af1e5e009c3fe1
Sebastian Günther <planung@ep-kolar.at>
2026-04-25 22:43:19 +0200

    v3.8.61 bump: Performance-Audit (TicketListItem.memo) + Schema-Doku + 33 neue Tests

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.8.60
-const CACHE_NAME = "epkolar-v3.8.60";
+// EP Kolar Service Worker v3.8.61
+const CACHE_NAME = "epkolar-v3.8.61";
 const ASSETS = [
   './',
   './index.html'
