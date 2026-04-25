81735ec19ba238538c9d527145233a33e418df69
Sebastian Günther <planung@ep-kolar.at>
2026-04-25 22:04:45 +0200

    v3.8.59 bump: Schema-Hotfix + Code-Quality-Marathon (silent-catch + console)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.8.57
-const CACHE_NAME = "epkolar-v3.8.57";
+// EP Kolar Service Worker v3.8.59
+const CACHE_NAME = "epkolar-v3.8.59";
 const ASSETS = [
   './',
   './index.html'
