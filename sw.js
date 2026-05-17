f5d90d654a38e20575172436167fa5e943b442cb
Sebastian Günther <planung@ep-kolar.at>
2026-05-17 19:22:33 +0200

    v3.9.0 multi: Modal-Z-Index-Fix (FFF) + Mangel-Submit-Double-Click-Prevention (GGG)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.8.99
-const CACHE_NAME = "epkolar-v3.8.99";
+// EP Kolar Service Worker v3.9.0
+const CACHE_NAME = "epkolar-v3.9.0";
 const ASSETS = [
   './',
   './index.html'
