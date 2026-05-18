26e34007c9b29c87b1e7ede7ca6f4d0e82ee7c55
Sebastian Günther <planung@ep-kolar.at>
2026-05-18 09:23:32 +0200

    v3.9.12 perf: AbortSignal.timeout fallback für iOS Safari <15 / alte Chrome (S15-4)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.9.11
-const CACHE_NAME = "epkolar-v3.9.11";
+// EP Kolar Service Worker v3.9.12
+const CACHE_NAME = "epkolar-v3.9.12";
 const ASSETS = [
   './',
   './index.html'
