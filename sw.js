1c1b330b99385540351d69f9226233b0df8842b7
Sebastian Günther <planung@ep-kolar.at>
2026-05-18 13:23:37 +0200

    v3.9.16 quality: async-catches logged + JSON.parse safe-wrap + localStorage try-catch (Sprint 1 Code-Quality)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.9.15
-const CACHE_NAME = "epkolar-v3.9.15";
+// EP Kolar Service Worker v3.9.16
+const CACHE_NAME = "epkolar-v3.9.16";
 const ASSETS = [
   './',
   './index.html'
