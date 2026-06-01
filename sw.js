a2ef18f5f9b22a6350d819534d34a31ba15f2b41
Sebastian Günther <planung@ep-kolar.at>
2026-06-01 19:50:14 +0200

    v3.9.49 Projekt-Premium-Polish: VPlan Pin-Color-System (status-aware orange/blau/grün/rot) + Layer-Pills + TicketDetail Status-Workflow-Pills + Progress-Slider Visual + Hover-Tooltip, VMang Card-Layout Status-Badges + Photo-Strip + Quick-Action-Pills, Projekt-Header Progress-Bar + Quick-Stats + Quick-Actions (Sebastian: smarter/interaktiver/nicht-fad). TIME_HOUR konstante statt 3600000-magic

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.9.48
-const CACHE_NAME = "epkolar-v3.9.48";
+// EP Kolar Service Worker v3.9.49
+const CACHE_NAME = "epkolar-v3.9.49";
 const ASSETS = [
   './',
   './index.html'
