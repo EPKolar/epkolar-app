2c37b6ee3317bbd3ea30df41f6a070bbce511e63
Sebastian Günther <planung@ep-kolar.at>
2026-06-02 07:24:44 +0200

    v3.9.59 version-string sync: index.html (SW_VER + APP_VERSION) + sw.js (header + CACHE_NAME) auf 3.9.59 aktualisiert (v3.9.59 226e586 hatte nur SQL-Datei, Version-Strings vergessen).

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.9.58
-const CACHE_NAME = "epkolar-v3.9.58";
+// EP Kolar Service Worker v3.9.59
+const CACHE_NAME = "epkolar-v3.9.59";
 const ASSETS = [
   './',
   './index.html'
