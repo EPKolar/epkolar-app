183c5b7cdcd29939e1dedce6f35cd397e191bccf
Sebastian Günther <planung@ep-kolar.at>
2026-06-02 19:35:09 +0200

    v3.9.71 LAUNCH PlanRadar-R3: VPlan Hover-Quick-Info + Pin-Cluster + ProjList Card-Polish + Mobile-FAB (Sebastian: Projekte ist schwach, schau PlanRadar)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.9.70
-const CACHE_NAME = "epkolar-v3.9.70";
+// EP Kolar Service Worker v3.9.71
+const CACHE_NAME = "epkolar-v3.9.71";
 const ASSETS = [
   './',
   './index.html'
