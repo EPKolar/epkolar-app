5559e43964f69f0532ab1dab443669301640c53e
Sebastian Günther <planung@ep-kolar.at>
2026-06-02 12:19:34 +0200

    v3.9.62 A11Y-Round-3 + Mobile-Touch-Residuals (12 findings / 12 fixes)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.9.61
-const CACHE_NAME = "epkolar-v3.9.61";
+// EP Kolar Service Worker v3.9.62
+const CACHE_NAME = "epkolar-v3.9.62";
 const ASSETS = [
   './',
   './index.html'
