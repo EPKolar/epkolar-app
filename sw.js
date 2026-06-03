7d6c3b6396b4728e6a60e844334a427c715760be
Sebastian Günther <planung@ep-kolar.at>
2026-06-03 06:44:52 +0200

    v3.9.89 Sprint-68 Mobile-Tap-Adoption + Link/Form-Field UX-Refinement
    - 68.1 epk-tap applied to 10 clickable sites (ContextMenu-Item, User/Customer/Project/Ticket cards, Ticket-back-swipe, Fink status filter + stunden reset, MA/FZ toggle spans)
    - 68.2 epk-link applied to 5 anchor tags (tel: + mailto: + http: in customer/office contact cards)
    - 68.3 epk-form-field applied to 4 inputs (Login user + pw, Password-reset, New-user pw)
    
    Verify:
    - pytest 502/502 green
    - bracket pre+post identical: () -2 / {} 0 / [] 0
    - class counts: epk-tap 13 (was 3), epk-link 8 (was 3), epk-form-field 9 (was 5)
    - 19 net adoptions = 14 Tier-1 (well above 8 min)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EP Kolar Service Worker v3.9.88
-const CACHE_NAME = "epkolar-v3.9.88";
+// EP Kolar Service Worker v3.9.89
+const CACHE_NAME = "epkolar-v3.9.89";
 const ASSETS = [
   './',
   './index.html'
